afc4a48a2731b7b11d0747037d00b22c2519ed60
webhugojustforpage <18940874730@163.com>
2016-12-27 13:44:27 +0800

    回复功能

--- router/weixin/center.js
+++ router/weixin/center.js
@@ -5,7 +5,7 @@ const Signature = require('../../package.json').signature;
 const responser = require('./../../lib/responser');
 const WeixinConfig = require('../../config/config').weixin;
 const request = require('superagent');
-const xml = require('../../lib/xml').xml;
+const xml = require('../../lib/xml');
 
 module.exports = router => {
     router.get("/weixin/getAuthentic", async(ctx, next) => {
