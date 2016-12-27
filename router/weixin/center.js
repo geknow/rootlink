001e57b6c25058eab87ca934c6101cdb32c67efe
webhugojustforpage <18940874730@163.com>
2016-12-27 14:01:15 +0800

    回复功能

--- router/weixin/center.js
+++ router/weixin/center.js
@@ -6,26 +6,41 @@ const responser = require('./../../lib/responser');
 const WeixinConfig = require('../../config/config').weixin;
 const request = require('superagent');
 const xml = require('../../lib/xml');
+var crypto = require("crypto");
+
+const checkSignature = function (query) {
+    var timestamp = query.timestamp
+        , nonce = query.nonce
+        , signature = query.signature;
+
+    var tmpArr = [timestamp, nonce, Signature];
+    tmpArr.sort();
+    var tmpStr = `${tmpArr[0]}${tmpArr[1]}${tmpArr[2]}`;
+
+    const hash = crypto.createHash("sha1");
+    var encrypted = hash.update(tmpStr, "utf8").digest("hex");
+    return encrypted === signature;
+};
 
 module.exports = router => {
-    router.get("/weixin/getAuthentic", async(ctx, next) => {
+    router.get("/weixin", async(ctx, next) => {
         console.log("Authentic");
         let query = ctx.request.query;
-        let signature = query.signature;
-        let timestamp = query.timestamp;
-        let nonce = query.nonce;
         let echostr = query.echostr;
-        // if(signature != Signature){
-        //     responser.reject(ctx);
-        //     return;
-        // }
-        ctx.body = echostr;
+        ctx.body = checkSignature(query) ? echostr : "invalid";
     });
 
 
-    router.post("/weixin/getAuthentic", async (ctx, next) => {
+    router.post("/weixin", async(ctx, next) => {
         let data = await xml(ctx);
         console.log(data);
+        if (data.MsgType == "text") {//文本信息
+
+        }else if(data.MsgType == "voice"){//语音信息
+
+        }else{//其他信息忽略
+            ctx.body = "";
+        }
         ctx.body = "";
     });
 
